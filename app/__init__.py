f355da9c7f217a613f195fe132bd61299bda6c4c
vagrant <vagrant@precise64.(none)>
2016-08-22 19:46:12 +0000

    Completed login

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
 
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import os
-from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID
+from flask_login import LoginManager
+from flask_openid import OpenID
 from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+lm = LoginManager()
+
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
 
-lm = LoginManager()
+
 lm.init_app(app)
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
+lm.login_view = 'login'
